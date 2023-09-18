1997019da5118e7795278a9bc8af9a3c89278631
Shi Ni Wong <shini.wong@ncs.com.sg>
2023-09-18 13:30:07 +0800

    basic registration page

--- src/app/registration/component/registration.component.ts
+++ src/app/registration/component/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class RegistrationComponent {
 
+  userType!: string;
+  name!: string;
+  email!: string;
+  password!: string;
+  confirmPassword!: string;
+  errorMessage: string = '';
+
+  constructor() {}
+
+  onSubmit() {
+    //validation and registration logic
+  }
+
 }
+
+
