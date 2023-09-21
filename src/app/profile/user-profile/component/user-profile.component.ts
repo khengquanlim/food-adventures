08b5fbba0f6849134db892c3e1f912159e66d52a
Pang Shu Wen <pangshuwen99@gmail.com>
2023-09-21 14:48:18 +0800

    user profile details

--- src/app/profile/user-profile/component/user-profile.component.ts
+++ src/app/profile/user-profile/component/user-profile.component.ts
@@ -13,4 +13,25 @@ export class UserProfileComponent implements OnInit {
   ngOnInit(): void {
     
   }
+  editMode = false;
+  user: any = {  // Initialize with user data
+    name: 'John Doe',
+    age: 30,
+    gender: 'Male',
+    likes: 'Reading, Hiking',
+    dislikes: 'Crowded places',
+    profilePictureUrl: 'assets/default-profile.jpg'  // Default profile picture
+  };
+
+  toggleEditMode() {
+    this.editMode = !this.editMode;
+  }
+  onProfilePictureChange(event: any) {
+    // Implement logic to upload and set the new profile picture
+    const file = event.target.files[0];
+    // Implement logic to upload the file and set profilePictureUrl
+  }
+  
+
+  saveChanges() {}
 }
