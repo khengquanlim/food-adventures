b04f445f3e92c6cfabc788ad5c783ae2a90bbd40
Pang Shu Wen <pangshuwen99@gmail.com>
2023-09-29 19:12:32 +0800

    login html + css, navbar revamp, user-profile functionality

--- src/app/profile/user-profile/component/user-profile.component.ts
+++ src/app/profile/user-profile/component/user-profile.component.ts
@@ -25,20 +25,43 @@ export class UserProfileComponent implements OnInit {
     gender: 'Male',
     likes: 'Reading, Hiking',
     dislikes: 'Crowded places',
-    profilePictureUrl: 'assets/default-profile.jpg'  // Default profile picture
+    profilePictureUrl: 'assets/images/ppg.jpg'  // Default profile picture
   };
   profileName="Mary";
   profileAge=36;
   profileGender="Female";
+
+  imageUrls: string[] = [
+    'assets/images/avocado.jpeg',
+    'assets/images/cake.jpeg',
+    'assets/images/crepe.jpeg',
+    'assets/images/pancake.jpeg',
+    'assets/images/pasta.jpeg',
+    'assets/images/potato.jpeg',
+    'assets/images/steak.jpeg',
+    // Add jpeg image URLs as needed
+  ];
+
+  numberOfColumns=3;
 
   toggleEditMode() {
     this.editMode = !this.editMode;
     console.log("editmode: ", this.editMode);
   }
   onProfilePictureChange(event: any) {
+    console.log('File input changed:', event);
     // Implement logic to upload and set the new profile picture
     const file = event.target.files[0];
     // Implement logic to upload the file and set profilePictureUrl
+    if (file && file.type.startsWith('image/')) {
+      console.log("in true");
+      this.user.profilePictureUrl = URL.createObjectURL(file);
+  } else {
+    console.log('Invalid file type or no file selected.');
+  }
+    
+    
+    console.log('File input changed:', event);
   }
   
 
