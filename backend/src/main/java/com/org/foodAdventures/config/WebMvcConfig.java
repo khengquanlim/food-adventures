bccd2dd3f620704a1ed06b9122431a7002334719
Sky Lee <jetxuen_lee@mymail.sutd.edu.sg>
2023-10-16 22:28:19 +0800

    solved CORS issue

---
+++ backend/src/main/java/com/org/foodAdventures/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.org.foodAdventures.config;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.*;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST","OPTIONS")
+                .maxAge(3600);
+    }
+}
