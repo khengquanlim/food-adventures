3939b9110b46e10ece106b88556857b489efbf30
Sky Lee <jetxuen_lee@mymail.sutd.edu.sg>
2023-10-25 18:10:51 +0800

    added mock url for BE microservices

--- backend/src/main/java/com/org/foodAdventures/config/WebMvcConfig.java
+++ backend/src/main/java/com/org/foodAdventures/config/WebMvcConfig.java
@@ -1,18 +1,16 @@
 package com.org.foodAdventures.config;
 
-import java.io.IOException;
 
 import com.org.foodAdventures.common.*;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.*;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {g
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
                 .allowedOrigins(CommonConstant.PROD_URL)
